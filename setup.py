63a49817981a631dfb40e1f9049c25ec4b22c222
Charles Perkins <chuck@kuracali.com>
2018-03-22 08:45:13 -0700

    define basic python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='yo',
+      version='0.1',
+      description='Identity Services over chff',
+      url='http://github.com/mnes-io/yo',
+      author='Charles Perkins',
+      author_email='charlesap@gmail.com',
+      license='MIT',
+      packages=['yo'],
+      zip_safe=False)
